382e1e7c741eb72fb6109b94ba3768b4b6cbf919
MajicGit <95971895+MajicGit@users.noreply.github.com>
2022-06-16 23:12:55 +0200

    Sorting the list as well

--- jtv_lists.py
+++ jtv_lists.py
@@ -1,6 +1,7 @@
 import requests
 import json
 videos = {}
+print("Querying for videos")
 for year in ["2021","2022"]:
     for week in range(0,52):
         try:
@@ -19,14 +20,15 @@ for year in ["2021","2022"]:
                     videos[video_id] = videos[video_id] + 1
                 else:
                     videos[video_id] = 1 
-
+print("Got all the videos, saving to file")
 with open("Output.txt","w") as file:
     file.write("Video ID, Number of times played \n")
-    for video_id in videos:
-        file.write(f"{video_id}, {videos[video_id]} \n")
+    for video_id,num_plays in sorted(videos.items(),key= lambda x:x[1],reverse=True): #Sort videos by number of plays
+        file.write(f"{video_id}, {num_plays} \n")
 
+print("If too lazy to control f:")
 while True:
-    video = input("Video Id")
+    video = input("Video Id: ")
     if video in videos:
         print("Video has been played")
     else:
