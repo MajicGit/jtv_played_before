ccb3ce18d15130704981a8f5c8a43e4a020df6a5
MajicGit <95971895+MajicGit@users.noreply.github.com>
2022-06-16 23:58:00 +0200

    Updated readme, added some more info

--- jtv_lists.py
+++ jtv_lists.py
@@ -1,9 +1,9 @@
 import requests
 import json
 videos = {}
-print("Querying for videos")
+print("Querying for videos, this may take a while")
 for year in ["2021","2022"]:
-    for week in range(0,52):
+    for week in range(1,53):
         try:
             r = requests.get(f"https://jungletv.live/raffles/weekly/{year}/{week}/tickets#",timeout=2)
         except:
@@ -26,7 +26,7 @@ with open("Output.txt","w") as file:
     for video_id,num_plays in sorted(videos.items(),key= lambda x:x[1],reverse=True): #Sort videos by number of plays
         file.write(f"{video_id}, {num_plays} \n")
 
-print("If too lazy to control f:")
+print("Infinite querying of specific Video IDs:")
 while True:
     video = input("Video Id: ")
     if video in videos:
